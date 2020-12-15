5f162f695945d7b8f196ff74694c9b049880cfdf
xxggxxgan <34018070+xxggxxgan@users.noreply.github.com>
2020-12-15 17:54:59 -0500

    fix bugs

--- src/ui/LoanPage.java
+++ src/ui/LoanPage.java
@@ -43,7 +43,7 @@ public class LoanPage implements IPages{
         if(systemManager.getCurrentUser().hasLoanAccount()){
             systemManager.chooseAccount(AccountType.LOAN.getAccountType());
         }else{
-            //TODO:这块最好是可以选择贷款利率的
+            //TODO:It is best to choose the loan interest rate
             systemManager.createLoanAccount(LoanDao.DAY_RATE);
         }
 
