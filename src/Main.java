c5187204bd6baca321aed7492cc891fb7c934db1
unknown <wangkk@bu.edu>
2020-12-15 16:33:03 -0500

    OOP optimization

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,20 @@ import manager.entity.Result;
 import manager.user.Consumer;
 import manager.user.Manager;
 import manager.user.UserManager;
+import ui.WelcomePage;
 
 public class Main {
 
     public static void main(String[] args) {
-        UserManager userManager = UserManager.getInstance();
-        Result<Consumer> consumer = userManager.getConsumer("test1", "test1");
-        System.out.println(consumer);
-        Result<Manager> manager = userManager.getManager("test", "test");
-        System.out.println(manager);
-        Result<Consumer> consumerResult = userManager.registerConsumer("123", "123");
-        System.out.println(consumerResult);
-        consumerResult = userManager.registerConsumer("test1", "123");
-        System.out.println(consumerResult);
-
-
+//        UserManager userManager = UserManager.getInstance();
+//        Result<Consumer> consumer = userManager.getConsumer("test1", "test1");
+//        System.out.println(consumer);
+//        Result<Manager> manager = userManager.getManager("test", "test");
+//        System.out.println(manager);
+//        Result<Consumer> consumerResult = userManager.registerConsumer("123", "123");
+//        System.out.println(consumerResult);
+//        consumerResult = userManager.registerConsumer("test1", "123");
+//        System.out.println(consumerResult);
+        new WelcomePage();
     }
 }
